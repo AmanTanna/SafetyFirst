a1885a8c24373e05535206b70158fe65cf29be2a
Michael Kwan <michaelkwan1238@gmail.com>
2024-02-17 12:58:36 -0500

    Initialize project

---
+++ safetyfirst/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>There is now even more text in order for the app to function and even more and we can keep going</Text>
+      <Text>This is yet another line of text that I can place down into this app</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
