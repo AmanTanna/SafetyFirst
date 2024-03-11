3f682d21fa7066ba953f82d236bc28843791d820
AmanTanna <122839949+AmanTanna@users.noreply.github.com>
2024-03-11 18:21:42 -0400

    Update App.js

--- safetyfirst/App.js
+++ safetyfirst/App.js
@@ -6,7 +6,10 @@ import { useForm } from 'react-hook-form';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { FAB } from 'react-native-paper';
-import {AddressToCoords} from './data/ConvertCoords.js'
+//import {AddressToCoords} from './data/ConvertCoords.js'
+
+
+
 
 
 
@@ -49,7 +52,44 @@ const AnotherScreen = ({AddressToCoords}) => {
     console.log(helper, helper2, helper3, helper4, helper_new);
     // Here you can perform further actions with the form data, like sending it to a server
     navigation.navigate('Home'); // Navigate back to the Home screen
-    //console.log(AddressToCoords(helper2, "key")[0].lat, AddressToCoords(helper2, "key")[0].lng)
+    //console.log(AddressToCoords(helper2, "65d174a784a72523862703trh8e62ef")[0].lat, AddressToCoords(helper2, "65d174a784a72523862703trh8e62ef")[0].lng)
+     //Test the code now
+     function AddressToCoords(address, api_key) {
+       /**
+        * Take as input an address, then return a promise that can be resolved datawise, holding the lat and lon values
+        * data[0].lat
+        * data[0].lon
+        */
+       var addr = address.replace(/ /g, "+");
+
+
+       try {
+         const response =  fetch(
+           'https://geocode.maps.co/search?q=University+of+Waterloo&api_key=65e16d75c022f644333604ikhb986a9'
+         );
+
+
+         if (!response.ok) {
+           throw new Error("Could not fetch resource");
+         }
+
+         const data =   response.json();
+         return data;
+       } catch (error) {
+         console.error("Error, could not attempt fetch of maps");
+       }
+     }
+     var key = "65d174a784a72523862703trh8e62ef";
+     var cds = AddressToCoords("University of Waterloo", "65d174a784a72523862703trh8e62ef");
+     //var addr = CoordsToAddress(43.71697314176533, -79.33889076504421, key);
+     console.log("Getting Coordinates for University of Waterloo");
+     cds.then((data) => {
+       console.log(data);
+     });
+//     console.log("Getting Address for Ontario Science Center");
+//     addr.then((data) => {
+//       console.log(data);
+//     });
     }
 
   return (
